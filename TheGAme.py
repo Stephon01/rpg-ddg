0d2ff765939c652e3028862836d7e8f756c25c46
Stephon01 <54902003+Stephon01@users.noreply.github.com>
2019-11-04 09:18:32 -0600

    error fix

--- TheGAme.py
+++ TheGAme.py
@@ -2,15 +2,3 @@
 ```
 python thegame.py
 ```
-    doc =py
-    ```perty."
-    def fget(self):
-        return py
-        ```f fset(self, value):
-        py
-        ```alue
-    def fdel(self):
-        del py
-        ```turn locpy 
-        ```roppy 
-        ```
